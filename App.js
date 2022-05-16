78b5ef06b1e8d633805108d02fc7cbb56806d783
Bust <pan1204320289@gmail.com>
2022-05-16 11:37:31 +0800

    import ui-kitten

--- App.js
+++ App.js
@@ -3,38 +3,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
 import Emergency from './components/Emergency';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components';
+import { BottomNavigation, BottomNavigationTab, IconRegistry, Icon } from '@ui-kitten/components';
+import { default as theme } from './theme.json';
 
-const Tab = createBottomTabNavigator();
+const HomeIcon = (props) => (
+  <Icon {...props} name='home-outline'/>
+);
+
+const EmergencyIcon = (props) => (
+  <Icon {...props} name='thermometer-outline'/>
+);
+
+const { Navigator, Screen } = createBottomTabNavigator();
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}>
+    <BottomNavigationTab title='Home'  icon={HomeIcon}/>
+    <BottomNavigationTab title='Emergency'  icon={EmergencyIcon}/>
+  </BottomNavigation>
+);
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />} screenOptions={{headerShown: false}}>
+    <Screen name='Home' component={Home}/>
+    <Screen name='Emergency' component={Emergency}/>
+  </Navigator>
+);
+
+
 export default function App() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-            screenOptions={{
-              tabBarActiveTintColor:'white',
-              tabBarStyle: { backgroundColor: '#3E4F7A'},
-              headerShown: false,
-            }}
-            >
-                <Tab.Screen
-                name='Home'
-                component={Home}
-                options={{
-                  tabBarIcon: ({focused}) => (
-                    <Icon name='home' color={focused ? 'white' : 'lightgrey'} size={30} />
-                  )
-                }}
-            ></Tab.Screen>
-            <Tab.Screen
-                name='Emergency'
-                component={Emergency}
-                options={{
-                  tabBarIcon: ({focused}) => (
-                    <Icon name='ambulance' color={focused ? 'white' : 'lightgrey'} size={30} />
-                  )
-                }}
-            ></Tab.Screen>
-            </Tab.Navigator>
-        </NavigationContainer>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+          <NavigationContainer>
+            <TabNavigator/>
+          </NavigationContainer>
+      </ApplicationProvider>
+    </>
+
     )
 }
