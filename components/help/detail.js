2385f2ae857264a7b5ba69b8d574a9067f859d0a
Dysprosium <1119493091@qq.com>
2022-05-24 21:26:23 +0800

    modify help

--- components/help/detail.js
+++ components/help/detail.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet, ScrollView, View } from 'react-native';
 import { Button, Card, ListItem, List, StyleService, Divider, Icon, Input, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import { styles } from '../Home';
+import { getImageSourceProperties } from 'react-native/Libraries/Image/ImageSource';
 
 const BackIcon = (props) => (
   <Icon {...props} name='arrow-back' />
@@ -15,11 +16,99 @@ const DATA = new Array(8).fill({
 
 export const Detail = ({ navigation, route }) => {
   const {item} = route.params;
+  if(item.category =='Medicine') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Medicine0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Medicine1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Medicine2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Medicine3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Medicine4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Medicine5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Medicine6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Medicine7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Medicine8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Medicine9.jpg');
+  } else if(item.category == 'Nursery') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Nursery0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Nursery1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Nursery2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Nursery3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Nursery4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Nursery5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Nursery6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Nursery7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Nursery8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Nursery9.jpg');
+  } else if(item.category == 'Service') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Service0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Service1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Service2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Service3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Service4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Service5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Service6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Service7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Service8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Service9.jpg');
+  } else if(item.category == 'Food') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Food0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Food1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Food2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Food3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Food4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Food5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Food6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Food7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Food8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Food9.jpg');
+  } else if(item.category == 'Electronic products') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/ep0.jpg');
+    if(rand == 1)   imagePath = require('./extra/ep1.jpg');
+    if(rand == 2)   imagePath = require('./extra/ep2.jpg');
+    if(rand == 3)   imagePath = require('./extra/ep3.jpg');
+    if(rand == 4)   imagePath = require('./extra/ep4.jpg');
+    if(rand == 5)   imagePath = require('./extra/ep5.jpg');
+    if(rand == 6)   imagePath = require('./extra/ep6.jpg');
+    if(rand == 7)   imagePath = require('./extra/ep7.jpg');
+    if(rand == 8)   imagePath = require('./extra/ep8.jpg');
+    if(rand == 9)   imagePath = require('./extra/ep9.jpg');
+  }else if(item.category == 'Pet supplies') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Pet0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Pet1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Pet2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Pet3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Pet4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Pet5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Pet6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Pet7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Pet8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Pet9.jpg');
+  } else if(item.category == 'others') {
+    rand = parseInt(item.needId/100)%10;
+    if(rand == 0)   imagePath = require('./extra/Others0.jpg');
+    if(rand == 1)   imagePath = require('./extra/Others1.jpg');
+    if(rand == 2)   imagePath = require('./extra/Others2.jpg');
+    if(rand == 3)   imagePath = require('./extra/Others3.jpg');
+    if(rand == 4)   imagePath = require('./extra/Others4.jpg');
+    if(rand == 5)   imagePath = require('./extra/Others5.jpg');
+    if(rand == 6)   imagePath = require('./extra/Others6.jpg');
+    if(rand == 7)   imagePath = require('./extra/Others7.jpg');
+    if(rand == 8)   imagePath = require('./extra/Others8.jpg');
+    if(rand == 9)   imagePath = require('./extra/Others9.jpg');
+  }
   const [inputComment, setInputComment] = useState();
   const [comments, setComments] = useState();
+//   const [imagePath, setImagePath] = useState();
+  var imagePath;
   const user = [];
   useEffect(() => {
-    fetchData()
+    fetchData();
+    
     }, []); 
 
   async function fetchData() {
@@ -31,6 +120,9 @@ export const Detail = ({ navigation, route }) => {
     //     let resUser = await getUser.json();
     //     user.push(resUser)
     // }
+    for(var comment of resComment) {
+        comment.createdTime = comment.createdTime.slice(0, 10) +' '+comment.createdTime.slice(11, 16);
+    }
     console.log(resComment)
     // console.log("user")
     // console.log(user)
@@ -47,9 +139,8 @@ export const Detail = ({ navigation, route }) => {
   
   const renderItemHeader = (headerProps, info) => (
     <View {...headerProps}>
-      <Text category='h6'>
-        {info.title}
-      </Text>
+      <Text category='h6'>{info.user.userName}</Text>
+      <Text category='p2' appearance='hint'>{info.createdTime}</Text>
     </View>
   );
 
@@ -90,43 +181,46 @@ export const Detail = ({ navigation, route }) => {
     //   style={styles.item}
       status='basic'
       header={headerProps => renderItemHeader(headerProps, item)}
-      footer={() => renderItemFooter(item)}>
-      <Text>{index+1}</Text>
+    //   footer={() => renderItemFooter(item)}>
+      >
       <Text>{item.commentText}</Text>
-      <Text>{item.createdTime}</Text>
     </Card>
     );
 
   return (
     <Layout style={{flex:1}}>
-    <SafeAreaView>
-      <TopNavigation title='Need Detail' alignment='center' accessoryLeft={BackAction}/>
-      <Divider/>
-      
-        <Text style={style.titleLabel} category='h4'>{item.title}</Text>
-        <Text style={style.descriptionLabel}>{item.category}</Text>
-        <Text style={style.contentLabel} category='s1'>{item.description}</Text>
-        {/* <ImageBackground style={styles.image} source={data.image}/> */}
-        
-        <View style={style.authoringContainer}>
-            <Text category='p2' appearance='hint'>{item.tel}</Text>
-            <Text category='p2' appearance='hint'>{item.timeLimit}</Text>
-        </View>
-        <Input
-            style={style.commentInput}
-            placeholder='Write your comment'
-            value={inputComment}
-            onChangeText={setInputComment}
-            accessoryRight={() => renderItemAccessory(inputComment)}
-        />
+        <SafeAreaView>
+        <TopNavigation title='Need Detail' alignment='center' accessoryLeft={BackAction}/>
         <Divider/>
-        <List 
-            style={{marginLeft: 5, marginRight:5}}
-            data={comments}
-            ItemSeparatorComponent={Divider}
-            renderItem={renderItem}
-        />
-    </SafeAreaView>
+            <ScrollView>
+            {/* <Text style={style.titleLabel} category='h4'>{item.title}</Text> */}
+            <Text style={style.titleLabel} category='h2'>Title</Text>
+                <Text style={style.descriptionLabel} category='h4'>{item.category}</Text>
+                <Text style={style.contentLabel} category='s1'>{item.description}</Text>
+                <ImageBackground style={style.image} source={imagePath}/>
+                <View style={style.authoringContainer}>
+                    <Text category='p2' appearance='hint'>{item.tel}</Text>
+                    <Text category='p2' appearance='hint'>{item.timeLimit}</Text>
+                </View>
+                <Text style={style.commentText} category='h4'>Comments</Text>
+                <Input
+                    textStyle={{minHeight: 420}}
+                    style={style.commentInput}
+                    placeholder='Write your comment'
+                    value={inputComment}
+                    onChangeText={setInputComment}
+                    accessoryRight={() => renderItemAccessory(inputComment)}
+                />
+                <Divider/>
+            <List  
+            
+                style={{marginLeft: 5, marginRight:5}}
+                data={comments}
+                ItemSeparatorComponent={Divider}
+                renderItem={renderItem}
+            />
+            </ScrollView>
+        </SafeAreaView>
     </Layout>
     
   );
@@ -146,19 +240,27 @@ const style = StyleSheet.create({
     },
     image: {
       height: 240,
+      marginTop: 20,
+      marginBottom: 40,
     },
     titleLabel: {
       marginHorizontal: 24,
-      marginVertical: 16,
+      marginVertical: 8,
     },
     descriptionLabel: {
-      margin: 24,
+      fontSize: 20,
+      marginHorizontal: 24,
+      marginTop: 8,
     },
     contentLabel: {
       margin: 24,
     },
     authoringContainer: {
       flexDirection: 'row',
+      marginHorizontal: 24,
+    },
+    commentText: {
+      marginTop: 10,
       marginHorizontal: 24,
     },
     dateLabel: {
@@ -171,7 +273,7 @@ const style = StyleSheet.create({
     },
     commentInput: {
       marginHorizontal: 24,
-      marginTop: 24,
+      marginTop: 10,
       marginBottom: 20,
     },
   });
--- components/help/help.js
+++ components/help/help.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Icon, TopNavigation, TopNavigationAction, Layout, Divider, List, ListItem } from '@ui-kitten/components';
+import { Text, Button, Icon, TopNavigation, TopNavigationAction, Layout, Divider, List, ListItem } from '@ui-kitten/components';
+import ImageOverlay from 'react-native-image-overlay';
+
+
+const StarIcon = (props) => (
+  <Icon {...props} name='message-square-outline'/>
+);
+
 
 export default function Help({ navigation }) {
   const [data, setData] = useState();
@@ -40,13 +47,108 @@ export default function Help({ navigation }) {
     )
   };
 
-  const renderItem = ({item}) => (
-    <ListItem
-      title={item.category}
-      description={item.description}
-      accessoryRight={() => renderItemAccessory(item)}
-    />
-  );
+  const renderItem = ({item}) => {
+    var imagePath;
+    if(item.category =='Medicine') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Medicine0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Medicine1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Medicine2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Medicine3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Medicine4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Medicine5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Medicine6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Medicine7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Medicine8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Medicine9.jpg');
+    } else if(item.category == 'Nursery') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Nursery0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Nursery1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Nursery2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Nursery3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Nursery4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Nursery5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Nursery6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Nursery7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Nursery8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Nursery9.jpg');
+    } else if(item.category == 'Service') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Service0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Service1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Service2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Service3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Service4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Service5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Service6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Service7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Service8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Service9.jpg');
+    } else if(item.category == 'Food') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Food0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Food1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Food2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Food3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Food4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Food5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Food6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Food7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Food8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Food9.jpg');
+    } else if(item.category == 'Electronic products') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/ep0.jpg');
+      if(rand == 1)   imagePath = require('./extra/ep1.jpg');
+      if(rand == 2)   imagePath = require('./extra/ep2.jpg');
+      if(rand == 3)   imagePath = require('./extra/ep3.jpg');
+      if(rand == 4)   imagePath = require('./extra/ep4.jpg');
+      if(rand == 5)   imagePath = require('./extra/ep5.jpg');
+      if(rand == 6)   imagePath = require('./extra/ep6.jpg');
+      if(rand == 7)   imagePath = require('./extra/ep7.jpg');
+      if(rand == 8)   imagePath = require('./extra/ep8.jpg');
+      if(rand == 9)   imagePath = require('./extra/ep9.jpg');
+    }else if(item.category == 'Pet supplies') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Pet0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Pet1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Pet2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Pet3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Pet4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Pet5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Pet6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Pet7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Pet8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Pet9.jpg');
+    } else if(item.category == 'others') {
+      rand = parseInt(item.needId/100)%10;
+      if(rand == 0)   imagePath = require('./extra/Others0.jpg');
+      if(rand == 1)   imagePath = require('./extra/Others1.jpg');
+      if(rand == 2)   imagePath = require('./extra/Others2.jpg');
+      if(rand == 3)   imagePath = require('./extra/Others3.jpg');
+      if(rand == 4)   imagePath = require('./extra/Others4.jpg');
+      if(rand == 5)   imagePath = require('./extra/Others5.jpg');
+      if(rand == 6)   imagePath = require('./extra/Others6.jpg');
+      if(rand == 7)   imagePath = require('./extra/Others7.jpg');
+      if(rand == 8)   imagePath = require('./extra/Others8.jpg');
+      if(rand == 9)   imagePath = require('./extra/Others9.jpg');
+    }
+    return ( 
+      <ImageOverlay
+        style={{marginTop:10}}
+        source={imagePath}
+
+        contentPosition='top'
+        // description={item.description}
+        // accessoryRight={() => renderItemAccessory(item)}
+        onPress={() => navigateDetail(item)}>
+        <Text style={{fontWeight: 'bold', fontSize: 30, textAlign:'left'}}>Title</Text>
+        <Text>{item.description}</Text>
+        <Button size='giant'style={{marginTop: 180, marginLeft:300}} onPress={() => navigateDetail(item)} appearance='ghost' accessoryLeft={StarIcon}></Button>
+      </ImageOverlay>)
+   };
+  
 
   return (
     <Layout style={styles.container} level='2'>
@@ -61,6 +163,7 @@ export default function Help({ navigation }) {
         ItemSeparatorComponent={Divider}
         renderItem={renderItem}
         style={{marginLeft: 5, marginRight:5}}
+        
       />
     </Layout>
   );
